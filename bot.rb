d05438bb48c39f9aec10403907def7301783e8e6
William Archimede <william.archimede@protonmail.com>
2020-05-07 18:50:13 +0200

    iterate

--- bot.rb
+++ bot.rb
@@ -8,6 +8,15 @@ height.times do
     row = gets.chomp # one line of the grid: space " " is floor, pound "#" is wall
 end
 
+#######################
+$pac_id = 0
+$x = 0
+$y = 0
+$px = 0
+$py = 0
+$done = true
+#######################
+
 # game loop
 loop do
     my_score, opponent_score = gets.split(" ").collect {|x| x.to_i}
@@ -27,15 +36,41 @@ loop do
         y = y.to_i
         speed_turns_left = speed_turns_left.to_i
         ability_cooldown = ability_cooldown.to_i
+
+        ############################################################
+        if mine do
+          $pac_id = pac_id
+          $x = x
+          $y = y
+        end
+        ############################################################
     end
     visible_pellet_count = gets.to_i # all pellets in sight
     visible_pellet_count.times do
         # value: amount of points this pellet is worth
         x, y, value = gets.split(" ").collect {|x| x.to_i}
+
+        ############################################################
+        if value > 1 and $done do
+          $done = false
+          $px = x
+          $py = y
+          break
+        end
+        ############################################################
     end
     
     # Write an action using puts
     # To debug: STDERR.puts "Debug messages..."
     
-    puts "MOVE 0 15 10" # MOVE <pacId> <x> <y>
+    # puts "MOVE 0 15 10" # MOVE <pacId> <x> <y>
+    ############################################################
+    $done = $x == $px and $y == $py 
+    puts "MOVE #{$pac_id} #{$px} #{$py}"
+    ############################################################
 end
+
+############################################################
+
+
+
